0ac0e6ea0357e7d8ddbb2e3da095cd7b703d0af9
alexey.kalentyev <alexey.kalentyev@50ohm.tech>
2020-04-23 23:30:32 +0700

    Рабочий дизайнер форм

--- Plugin_Kompas3D/Plugin3D.UI/SprocketForm.Designer.cs
+++ Plugin_Kompas3D/Plugin3D.UI/SprocketForm.Designer.cs
@@ -58,7 +58,8 @@
             this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.splitContainer1.Location = new System.Drawing.Point(8, 8);
+            this.splitContainer1.Location = new System.Drawing.Point(11, 10);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -83,8 +84,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(997, 366);
-            this.splitContainer1.SplitterDistance = 335;
+            this.splitContainer1.Size = new System.Drawing.Size(1329, 450);
+            this.splitContainer1.SplitterDistance = 446;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 2;
             // 
             // NumberOfTeethTextBox7
@@ -92,9 +94,10 @@
             this.NumberOfTeethTextBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.NumberOfTeethTextBox7.Location = new System.Drawing.Point(235, 165);
+            this.NumberOfTeethTextBox7.Location = new System.Drawing.Point(313, 203);
+            this.NumberOfTeethTextBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NumberOfTeethTextBox7.Name = "NumberOfTeethTextBox7";
-            this.NumberOfTeethTextBox7.Size = new System.Drawing.Size(84, 20);
+            this.NumberOfTeethTextBox7.Size = new System.Drawing.Size(110, 22);
             this.NumberOfTeethTextBox7.TabIndex = 14;
             // 
             // ExeavationDepthTextBox6
@@ -102,9 +105,10 @@
             this.ExeavationDepthTextBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ExeavationDepthTextBox6.Location = new System.Drawing.Point(235, 139);
+            this.ExeavationDepthTextBox6.Location = new System.Drawing.Point(313, 171);
+            this.ExeavationDepthTextBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ExeavationDepthTextBox6.Name = "ExeavationDepthTextBox6";
-            this.ExeavationDepthTextBox6.Size = new System.Drawing.Size(84, 20);
+            this.ExeavationDepthTextBox6.Size = new System.Drawing.Size(110, 22);
             this.ExeavationDepthTextBox6.TabIndex = 13;
             // 
             // CylinderThicknessTextBox5
@@ -112,9 +116,10 @@
             this.CylinderThicknessTextBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CylinderThicknessTextBox5.Location = new System.Drawing.Point(235, 113);
+            this.CylinderThicknessTextBox5.Location = new System.Drawing.Point(313, 139);
+            this.CylinderThicknessTextBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CylinderThicknessTextBox5.Name = "CylinderThicknessTextBox5";
-            this.CylinderThicknessTextBox5.Size = new System.Drawing.Size(84, 20);
+            this.CylinderThicknessTextBox5.Size = new System.Drawing.Size(110, 22);
             this.CylinderThicknessTextBox5.TabIndex = 12;
             // 
             // CircleThicknessTextBox4
@@ -122,9 +127,10 @@
             this.CircleThicknessTextBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CircleThicknessTextBox4.Location = new System.Drawing.Point(235, 87);
+            this.CircleThicknessTextBox4.Location = new System.Drawing.Point(313, 107);
+            this.CircleThicknessTextBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CircleThicknessTextBox4.Name = "CircleThicknessTextBox4";
-            this.CircleThicknessTextBox4.Size = new System.Drawing.Size(84, 20);
+            this.CircleThicknessTextBox4.Size = new System.Drawing.Size(110, 22);
             this.CircleThicknessTextBox4.TabIndex = 11;
             // 
             // HoleRadiusTextBox3
@@ -132,9 +138,10 @@
             this.HoleRadiusTextBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.HoleRadiusTextBox3.Location = new System.Drawing.Point(235, 61);
+            this.HoleRadiusTextBox3.Location = new System.Drawing.Point(313, 75);
+            this.HoleRadiusTextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.HoleRadiusTextBox3.Name = "HoleRadiusTextBox3";
-            this.HoleRadiusTextBox3.Size = new System.Drawing.Size(84, 20);
+            this.HoleRadiusTextBox3.Size = new System.Drawing.Size(110, 22);
             this.HoleRadiusTextBox3.TabIndex = 10;
             // 
             // CylinderRadiusTextBox2
@@ -142,9 +149,10 @@
             this.CylinderRadiusTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CylinderRadiusTextBox2.Location = new System.Drawing.Point(235, 35);
+            this.CylinderRadiusTextBox2.Location = new System.Drawing.Point(313, 43);
+            this.CylinderRadiusTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CylinderRadiusTextBox2.Name = "CylinderRadiusTextBox2";
-            this.CylinderRadiusTextBox2.Size = new System.Drawing.Size(84, 20);
+            this.CylinderRadiusTextBox2.Size = new System.Drawing.Size(110, 22);
             this.CylinderRadiusTextBox2.TabIndex = 9;
             // 
             // CircleRadiusTextBox1
@@ -152,18 +160,20 @@
             this.CircleRadiusTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.CircleRadiusTextBox1.Location = new System.Drawing.Point(235, 9);
+            this.CircleRadiusTextBox1.Location = new System.Drawing.Point(313, 11);
+            this.CircleRadiusTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CircleRadiusTextBox1.Name = "CircleRadiusTextBox1";
-            this.CircleRadiusTextBox1.Size = new System.Drawing.Size(84, 20);
+            this.CircleRadiusTextBox1.Size = new System.Drawing.Size(110, 22);
             this.CircleRadiusTextBox1.TabIndex = 8;
             // 
             // BuildButton
             // 
             this.BuildButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.BuildButton.Location = new System.Drawing.Point(235, 212);
+            this.BuildButton.Location = new System.Drawing.Point(313, 261);
+            this.BuildButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BuildButton.Name = "BuildButton";
-            this.BuildButton.Size = new System.Drawing.Size(84, 20);
+            this.BuildButton.Size = new System.Drawing.Size(111, 25);
             this.BuildButton.TabIndex = 7;
             this.BuildButton.Text = "Построить";
             this.BuildButton.UseVisualStyleBackColor = true;
@@ -174,9 +184,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 168);
+            this.label7.Location = new System.Drawing.Point(16, 207);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.Size = new System.Drawing.Size(152, 17);
             this.label7.TabIndex = 6;
             this.label7.Text = "Количество зубьев(n)";
             // 
@@ -186,9 +197,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 142);
+            this.label6.Location = new System.Drawing.Point(16, 175);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(166, 13);
+            this.label6.Size = new System.Drawing.Size(219, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "Глубина шпоночной выемки (Н)";
             // 
@@ -198,9 +210,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 116);
+            this.label5.Location = new System.Drawing.Point(16, 143);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.Size = new System.Drawing.Size(167, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Толщина цилиндра (Sц)";
             // 
@@ -210,9 +223,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 90);
+            this.label4.Location = new System.Drawing.Point(16, 111);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(203, 13);
+            this.label4.Size = new System.Drawing.Size(267, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Толщина внешней части звездочки (S)";
             // 
@@ -222,9 +236,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Location = new System.Drawing.Point(16, 79);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.Size = new System.Drawing.Size(146, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Радиус отверстия (r)";
             // 
@@ -234,9 +249,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Location = new System.Drawing.Point(16, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.Size = new System.Drawing.Size(155, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Радиус цилиндра (Rц)";
             // 
@@ -246,9 +262,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.Size = new System.Drawing.Size(245, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Радиус внешней окружности (Rокр)";
             // 
@@ -258,19 +275,21 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(17, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(507, 333);
+            this.pictureBox1.Size = new System.Drawing.Size(508, 333);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // SprocketForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 375);
+            this.ClientSize = new System.Drawing.Size(1183, 462);
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SprocketForm";
             this.Text = "Form1";
             this.splitContainer1.Panel1.ResumeLayout(false);
